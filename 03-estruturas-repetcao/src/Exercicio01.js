6689e140c56b53944eb9c6b574cc585d7170c0be
xXFoozer <germano_gomes@estudante.sesisenai.org.br>
2024-06-20 21:55:03 -0300

    Exercicios de repeticao

---
+++ 03-estruturas-repetcao/src/Exercicio01.js
@@ -0,0 +1,7 @@
+import leia from "readline-sync"
+
+var numero = leia.questionInt("SEU NUMERO: ")
+
+for(var i = 0; i <= numero; i++ ){
+    console.log(i)
+}
--- 03-estruturas-repetcao/src/exemplo.js
+++ 03-estruturas-repetcao/src/exemplo.js
@@ -1,31 +1,28 @@
 //EXEMPLO DE WHILE !!!
 
 
-import leia from "readline-sync"
+// import leia from "readline-sync"
 
-var nota = leia.questionFloat("DIGITE A NOTA: ")
-while(nota < 0.0 || nota > 10.0){
-    console.log("NOTA INVALIDA")
-    nota = leia.questionFloat("DIGITE A NOTA NOVAMENTE: ")
-}
-console.log("NOTA VALIDA")
-
-
-
-
-//var estaCarregando= true;
-// var porcentagem = 0;
-
-// while(estaCarregando ) {
-//     console.log(porcentagem +"% carregando... " );
-   
-//     if (porcentagem < 100){
-//         porcentagem +=5;
-
-//     }else {
-//         estaCarregando = false;
-        
-//     }
+// var nota = leia.questionFloat("DIGITE A NOTA: ")
+// while(nota < 0.0 || nota > 10.0){
+//     console.log("NOTA INVALIDA")
+//     nota = leia.questionFloat("DIGITE A NOTA NOVAMENTE: ")
 // }
+// console.log("NOTA VALIDA")
 
 //------------------------------------------
+
+//FOR
+
+// for(var i = 0; i <= 10; i++){
+// console.log(" VALOR DE I = "+ i);
+
+// }
+//----------------------------------------
+
+//DO....WHILE
+//  var j = 0
+// do{
+    
+// } while(j < 10);
+
