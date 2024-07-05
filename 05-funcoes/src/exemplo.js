146b808d4dc44d38f59cc73bca78693e3d34e9de
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2024-07-04 21:53:32 -0300

    05-AULA DE FUNÇOES

---
+++ 05-funcoes/src/exemplo.js
@@ -0,0 +1,30 @@
+import leia from "readline-sync"
+
+function mostrar(){
+    var nome= leia.question("digite o nome: ")
+
+    console.log("Olá, "+ nome)
+}
+
+function somar(){
+    var num1 = leia.questionInt("DIGITE O NUMERO: ")
+    var num2 = leia.questionInt("DIGITE O NUMERO: ")
+
+    var result = num1 + num2;
+    console.log("O RESULTADO É: "+ result)
+}
+
+function subtrair (num1, num2 ){
+    var result = num1 - num2
+    console.log("O RESULTADO É: "+ result)
+}
+function multi (num1, num2){
+    var result = num1 * num2
+    return result;
+}
+subtrair(35,200);
+var resultado = multi(20,30);
+console.log(resultado)
+
+somar();
+mostrar();
