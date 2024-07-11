ed10b5f13e333e6deb1b7b329a89c434756d8844
xXFoozer <germano_gomes@estudante.sesisenai.org.br>
2024-07-11 20:01:36 -0300

    termino

--- 03-estruturas-repetcao/src/Exercicio14.js
+++ 03-estruturas-repetcao/src/Exercicio14.js
@@ -1,15 +1,57 @@
-// A TERMINAR...
-
+// ESTUDANTES E NOTAS /---/ DIA 11 07 2024 FAZENDO COM ENSINAMENTOS NOVOS 
+// USANDO ARRAYS
 import leia from "readline-sync"
+var nomes =[]
+var medias =[]
+var nomeMaiorMedia;
+var maiorMedia;
 
-var aluno = leia.question("NOME ALUNO: ")
-var nota1;
-var nota2;
-var nota3;
 
-for(var i = 10; aluno < i;){
-    var nota1= leia.questionFloat("NOTA 1 ALUNO: ")
-    var nota2=leia.questionFloat("NOTA 2 ALUNO: ")
-    var nota3=leia.questionFloat("NOTA 3 ALUNO: ")
+var i = 0;
+while(i < 10){
+    nomes[i] = leia.question('DIGITE O NOME '+ (i + 1) +': ');
+    var somaNotas = 0.0;
     
+    for(var j = 1; j <= 3 ; j++){
+        somaNotas += leia.questionFloat("DIGITE NOTA 0"+j+":");
+    }
+    medias[i] = somaNotas / 3;
+
+
+    i++;
 }
+for(var i = 0; i < medias.length; i++){
+    if(!maiorMedia || medias[i] > maiorMedia){
+        maiorMedia = medias[i]
+        nomeMaiorMedia = nomes[i]
+    }
+}
+
+console.log("ESTUDANTE COM A MAIOR MEDIA FOI "+nomeMaiorMedia+" COM "+ media);
+// SEM ARRAY----------------------------------------------------
+
+
+// var nomeMaiorMedia;
+// var maiorMedia;
+
+// var i = 1;
+// while(i <= 10){
+//     var nome = leia.question('DIGITE O NOME '+ i +': ');
+//     var somaNotas = 0.0;
+    
+//     for(var j = 1; j <= 3 ; j++){
+//         somaNotas += leia.questionFloat("DIGITE NOTA 0"+j+":");
+
+
+//     }
+//     var media =somaNotas / 3;
+
+//     if(maiorMedia === undefined || media > maiorMedia){
+//         maiorMedia = media;
+//         nomeMaiorMedia = nome;
+//     }
+
+
+//     i++;
+// }
+// console.log("ESTUDANTE COM A MAIOR MEDIA FOI "+nomeMaiorMedia+" COM "+ media);
--- 03-estruturas-repetcao/src/Exercicio15.js
+++ 03-estruturas-repetcao/src/Exercicio15.js
@@ -1,1 +1,22 @@
-// NÃO FIZ :(
+import leia from "readline-sync"
+
+var qtdM = 0, qtdF = 0, somaTodasAlturas=0;
+var somaAlturasFemin = 0, maiorAltura, menorAltura; 
+
+for(var i = 0; i < 10; i++){
+    var genero = leia.keyInSelect(["MASCULINO","FEMININO"])+ 1;
+    var altura = leia.questionFloat("DIGITE A ALTURA: ");
+    somaTodasAlturas += altura;
+
+    if(genero === 2){
+        somaAlturasFemin += altura;
+        qtdF++
+    }
+
+    if(genero === 1){
+        qtdM++
+    }
+
+
+
+}
